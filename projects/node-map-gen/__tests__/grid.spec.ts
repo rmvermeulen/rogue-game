9746d87ada55c131c50e2c88b8652c6c9e96eba7
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2019-04-28 19:12:50 +0200

    failing test case: isolated cells

--- projects/node-map-gen/__tests__/grid.spec.ts
+++ projects/node-map-gen/__tests__/grid.spec.ts
@@ -1,11 +1,9 @@
 import {
+  contains,
   filter,
   flip,
-  length,
-  map,
   pipe,
   pluck,
-  prop,
   propEq,
   splitWhen,
   times,
@@ -88,6 +86,29 @@ describe.each([
         expect(grid.cellAt(x, y)).toMatchObject({ x, y });
       });
     });
+  });
+
+  it('does not have too many isolated cells', () => {
+    const { cells } = grid;
+    const isolatedCells = cells.reduce((total, cell) => {
+      const isPartOfRoom = pipe(
+        filter(
+          ({ x, y }: ICell) =>
+            Math.abs(cell.x - x) + Math.abs(cell.y - y) === 1,
+        ),
+
+        pluck('room'),
+        contains(cell.room),
+      )(cells);
+
+      return total + (isPartOfRoom ? 0 : 1);
+    }, 0);
+    console.log(`
+    all cells ${cells.length}
+    isolated  ${isolatedCells}
+    thing     ${isolatedCells / cells.length}
+    `);
+    expect(isolatedCells / cells.length).toBeLessThan(0.05);
   });
 
   it('can be displayed in the cli', () => {
