ff0a8ea4ad018e7eb41b24d07770769abb5dc7f1
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2019-04-29 16:34:44 +0200

    auto tests: run as if in CI

--- projects/node-map-gen/lint-staged.config.js
+++ projects/node-map-gen/lint-staged.config.js
@@ -2,5 +2,5 @@ module.exports = {
   'package.json': ['prettier-package-json --write', 'git add'],
   'src/**/*.ts': ['yarn lint --fix', 'git add'],
   '__tests__/**/*.ts': ['yarn lint:test --fix', 'git add'],
-  '**/*.ts': ['jest --findRelatedTests'],
+  '**/*.ts': ['jest --ci --findRelatedTests'],
 };
