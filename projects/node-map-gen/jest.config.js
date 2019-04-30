7c6b058a39d8355193be5a8368355e84c790292f
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2019-04-30 18:40:51 +0200

    Jest config update, add watchmode-toggle-config

--- projects/node-map-gen/jest.config.js
+++ projects/node-map-gen/jest.config.js
@@ -4,7 +4,7 @@ module.exports = {
   setupFilesAfterEnv: ['jest-extended', 'expect-more-jest'],
   verbose: true,
   collectCoverage: true,
-  coveragePathIgnorePatterns: ['setupJest', '.json'],
+  coveragePathIgnorePatterns: ['.json'],
   globals: {
     'ts-jest': {
       diagnostics: {
@@ -16,5 +16,9 @@ module.exports = {
     'jest-watch-typeahead/filename',
     'jest-watch-typeahead/testname',
     'jest-watch-suspend',
+    ['jest-watch-toggle-config', { setting: 'bail' }],
+    ['jest-watch-toggle-config', { setting: 'collectCoverage' }],
+    ['jest-watch-toggle-config', { setting: 'notify' }],
+    ['jest-watch-toggle-config', { setting: 'verbose' }],
   ],
 };
