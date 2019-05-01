5d5cb9c8d3284475b6edfe184bc2fca81c317236
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2019-05-01 17:07:52 +0200

    test for naive grid generator

--- projects/node-map-gen/__tests__/grid.spec.ts
+++ projects/node-map-gen/__tests__/grid.spec.ts
@@ -73,25 +73,6 @@ describe.each(
       });
     });
 
-    it('does not have too many isolated cells', () => {
-      const { cells } = grid;
-      const isolatedCells = cells.reduce((total, cell) => {
-        const isPartOfRoom = pipe(
-          filter(
-            ({ x, y }: ICell) =>
-              Math.abs(cell.x - x) + Math.abs(cell.y - y) === 1,
-          ),
-
-          pluck('room'),
-          contains(cell.room),
-        )(cells);
-
-        return total + (isPartOfRoom ? 0 : 1);
-      }, 0);
-      // expect(isolatedCells / cells.length).toBeLessThan(0.15);
-      expect(isolatedCells / cells.length).toBeLessThan(0.25);
-    });
-
     describe('static methods', () => {
       describe('static sort', () => {
         let sorted: ICell[];
