96645bcde16e002de2ec292178dcfd15e2c72c35
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2019-05-04 14:31:20 +0200

    Add comment notes for greedy algo

--- projects/node-map-gen/src/generators/greedy.ts
+++ projects/node-map-gen/src/generators/greedy.ts
@@ -4,5 +4,14 @@ import { ICell, IGridOptions } from '../grid';
 export interface IGreedyOptions extends IGridOptions {}
 
 export const generateCells = (options: IGreedyOptions): ICell[] => {
+  // pick a starting cell for each room
+  // must be unique
+  // must not be locked in on 4 sides
+  // remaining cells are the pool
+  // for each room
+  // if pool is empty break;
+  // find cells adjacent to cell in current room
+  // pick best cell, shrink pool
+  // add cell to list for the current room
   return [];
 };
