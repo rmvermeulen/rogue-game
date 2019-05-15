d849a3ba71e9826ffb93897dc906d905c2e99e17
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2019-05-15 17:37:16 +0200

    web-app: Fix test config

--- projects/web-app/jest.config.js
+++ projects/web-app/jest.config.js
@@ -1,13 +1,22 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['jest-extended', 'tsconfig-paths/register'],
   verbose: true,
   collectCoverage: true,
   coveragePathIgnorePatterns: ['setupJest'],
+  moduleNameMapper: {
+    '@utils/(.*)': '<rootDir>/src/utils/$1',
+    '@defs/(.*)': '<rootDir>/src/defs/$1.d.ts',
+    '@containers/(.*)': '<rootDir>/src/containers/$1',
+    '@components/(.*)': '<rootDir>/src/components/$1',
+    '@src/(.*)': '<rootDir>/src/$1',
+  },
   globals: {
     'ts-jest': {
       diagnostics: {
         warnOnly: true,
+        tsConfig: 'tsconfig.spec.json',
       },
     },
   },
