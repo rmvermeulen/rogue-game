e6577643a7e260f67613b8279d140106063d7ae0
Rasmus Vermeulen <vermeulen.rasmus@gmail.com>
2019-05-15 13:43:49 +0200

    web-app: remove placeholder test

--- projects/web-app/src/createLabel.spec.ts
+++
@@ -1,8 +0,0 @@
-import { createLabel } from './index';
-
-describe('createLabel', () => {
-  it('creates a html thing with text', () => {
-    const el = createLabel('foo-bar');
-    expect(el.textContent).toBe('foo-bar');
-  });
-});
